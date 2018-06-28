6d8671921510e1ff0ff1b270d08a5fedf6730bb3
Ahamed <ahamedali95@gmail.com>
2018-06-28 15:27:05 -0400

    generated premiliminary models: user, product_listing and category

---
+++ app/models/product_listing.rb
@@ -0,0 +1,2 @@
+class ProductListing < ApplicationRecord
+end
---
+++ db/migrate/20180628192124_create_product_listings.rb
@@ -0,0 +1,18 @@
+class CreateProductListings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_listings do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :value
+      t.string :exchange_item
+      t.string :location
+      t.datetime :data_posted
+      t.integer :rating
+      t.integer :user_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
