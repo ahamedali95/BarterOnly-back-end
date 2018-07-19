c09f6ecf7230eb214186d14c922c08b8854d00a1
Ahamed <ahamedali95@gmail.com>
2018-07-18 21:44:11 -0400

    added README

--- app/models/product_listing.rb
+++ app/models/product_listing.rb
@@ -4,9 +4,9 @@ class ProductListing < ApplicationRecord
 
   #validations
   validates :name, {presence: true}
-  validates :name, length: {minimum: 5, maximum: 26}
+  validates :name, length: {minimum: 5, maximum: 30}
   validates :description, {presence: true}
-  validates :description, length: {minimum: 10, maximum: 200}
+  validates :description, length: {minimum: 10, maximum: 300}
   validates :image, {presence: true}
   validates :condition, {presence: true}
   validates :location , {presence: true}
